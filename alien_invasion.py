06f6ccd4d753c89d303a76538fefd41e27a19c91
prdtex <patrickrice@live.com>
2020-02-15 13:56:50 -0600

    added ship/alien collision detection and related game logic

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
 
 def run_game():
 
@@ -14,6 +15,9 @@ def run_game():
 
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Create an instance to store game statistics
+    stats = GameStats(ai_settings)
 
     # Make a ship and define ship controls, make a group of bullets and aliens
     ship = Ship(ai_settings, screen)
@@ -28,9 +32,11 @@ def run_game():
 
         # Monitor the event queue
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, aliens)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
